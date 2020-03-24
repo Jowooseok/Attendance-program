db0708144c8c4c10417876e7933707d9a1bfc008
Jowooseok <whdntjrsla12@naver.com>
2020-03-24 17:54:27 +0900

    setState 적용

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,24 @@ class HelloWooseok extends StatefulWidget {
 }
 
 class _HelloWooseokState extends State<HelloWooseok> {
+  String _message = "Hello World";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _changeMessage),
       appBar: AppBar(
         title: Text(widget.title),
       ),
-        body : Text(widget.title,  style: TextStyle(fontSize: 30),));
+        body : Text(_message,  style: TextStyle(fontSize: 30),));
+  }
+
+  void _changeMessage() {
+    setState(() {
+      _message = "헬로 월드";
+    });
   }
 }
 
